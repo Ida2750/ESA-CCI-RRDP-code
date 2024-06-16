8540f925af4344e3782717112a2578fe8fd4e27e
Idalundtorp <70795109+Idalundtorp@users.noreply.github.com>
2024-06-16 21:37:15 +0200

    Update AWI_ULS_processing_final.py
    updated email

--- RRDPp/code/AWI_ULS/AWI_ULS_processing_final.py
+++ RRDPp/code/AWI_ULS/AWI_ULS_processing_final.py
@@ -9,7 +9,7 @@ measured at the Weddel Sea Antarctica by Alfred Wegener Institute
 # -- File info -- #
 __author__ = 'Ida Olsen'
 __contributors__ = 'Henriette Skorup'
-__contact__ = ['s174020@student.dtu.dk']
+__contact__ = ['ilo@dmi.dk']
 __version__ = '0'
 __date__ = '2023-06-12'
 
