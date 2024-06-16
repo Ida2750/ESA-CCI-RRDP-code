b76baf8455f97901767cad24fbab34b8326c3858
Idalundtorp <70795109+Idalundtorp@users.noreply.github.com>
2024-06-16 21:45:45 +0200

    Update NPEO_processing_final.py
    updated email

--- RRDPp/code/NPEO/NPEO_processing_final.py
+++ RRDPp/code/NPEO/NPEO_processing_final.py
@@ -8,7 +8,7 @@ measured at the North Pole Enviromental Observatory
 
 __author__ = 'Ida Olsen'
 __contributors__ = 'Henriette Skorup'
-__contact__ = ['s174020@student.dtu.dk']
+__contact__ = ['ilo@dmi.dk']
 __version__ = '0'
 __date__ = '2021-08-31'
 
